3e3086feb415de0c79ab190f771f8b071a1b9e83
muiezarif <36100308+muiezarif@users.noreply.github.com>
2023-01-23 16:17:45 +0500

    new commit

---
+++ digitvl/next.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+            '/': { page: '/' },
+            '/home': { page: '/home' }
+        }
+    },
+}
+module.exports = {
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            issuer: {
+                test: /\.(js|ts)x?$/,
+            },
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+};
